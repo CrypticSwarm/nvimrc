e45817cf83ddf9e680bc2332dee24aac7726fa57
CrypticSwarm <crypticswarm+git@gmail.com>
2024-08-18 23:57:20 -0500

    Add Slimux config
    Temporary addition until I can research a better method. Neovim likely
    has better support for async terminal execution.

--- init.lua
+++ init.lua
@@ -183,6 +183,17 @@ require('lazy').setup({
   {
     'tpope/vim-surround',
     commit = 'aeb933272e72617f7c4d35e1f003be16836b948d'
+  },
+  {
+    'esamattis/slimux', -- Temporary for now until figuring out a better way in nvim
+    commit = '7056d0e12d29411dbae9591cab0c5bb798a8ff6c',
+    config = function ()
+      vim.keymap.set('n', '<leader>r', ':SlimuxREPLSendLine<CR>', { desc = '[R]un line', silent = true })
+      vim.keymap.set('n', '<leader>e', 'vip:SlimuxREPLSendSelection<CR>', { desc = 'Run block', silent = true })
+      vim.keymap.set('v', '<leader>r', ':SlimuxREPLSendSelection<CR>', { desc = '[R]un selection', silent = true })
+      vim.keymap.set('n', '<leader>aa', ':SlimuxShellLast<CR>', { desc = 'Select shell', silent = true })
+      vim.keymap.set('n', '<leader>ai', ':SlimuxShellPrompt<CR>', { desc = 'Shell prompt', silent = true })
+    end
   }
 }, {
   ui = {
