20746e556c22e31fc65e3be1ae1ef24a52cf4e96
CrypticSwarm <crypticswarm+git@gmail.com>
2024-09-29 01:22:38 -0500

    Add Lua LSP configuration

--- init.lua
+++ init.lua
@@ -313,6 +313,24 @@ require('lazy').setup({
     end
   },
   {
+    -- Neovim Lua LSP configuration
+    'folke/lazydev.nvim',
+    commit = '491452cf1ca6f029e90ad0d0368848fac717c6d2',
+    ft = 'lua',
+    dependencies = {
+      {
+        'Bilal2453/luvit-meta',
+        commit = 'ce76f6f6cdc9201523a5875a4471dcfe0186eb60',
+        lazy = true
+      }
+    },
+    opts = {
+      library = {
+        { path = 'luvit-meta/library', words = { 'vim%.uv' } }
+      }
+    }
+  },
+  {
     -- Main LSP Configuration
     'neovim/nvim-lspconfig',
     commit = '056f569f71e4b726323b799b9cfacc53653bceb3',
