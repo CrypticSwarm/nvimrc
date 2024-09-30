0c5a9558fd8687497772d8b7679b162ed3a0a0fe
CrypticSwarm <crypticswarm+git@gmail.com>
2024-09-30 00:26:26 -0500

    Use lualine status line

--- init.lua
+++ init.lua
@@ -417,6 +417,13 @@ require('lazy').setup({
         },
       }
     end,
+  },
+  {
+    'nvim-lualine/lualine.nvim',
+    commit = 'b431d228b7bbcdaea818bdc3e25b8cdbe861f056',
+    config = function ()
+      require('lualine').setup()
+    end
   }
 }, {
   ui = {
