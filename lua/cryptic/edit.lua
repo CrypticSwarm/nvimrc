27032e51c700811e02b779aadee85b7ef7b92f20
CrypticSwarm <crypticswarm+git@gmail.com>
2024-10-08 01:25:22 -0500

    Extract edit focused plugins to a separate file

---
+++ lua/cryptic/edit.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    'tpope/vim-surround',
+    commit = 'aeb933272e72617f7c4d35e1f003be16836b948d'
+  },
+  {
+    'tpope/vim-repeat',
+    commit = '65846025c15494983dafe5e3b46c8f88ab2e9635'
+  },
+  {
+    'tommcdo/vim-exchange',
+    commit = 'd6c1e9790bcb8df27c483a37167459bbebe0112e'
+  },
+}
