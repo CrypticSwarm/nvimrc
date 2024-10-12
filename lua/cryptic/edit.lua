a66b98276dca2cc3c8d3ceeb0960221b91ee14a6
CrypticSwarm <crypticswarm+git@gmail.com>
2024-10-11 22:56:08 -0500

    Add UndoTree plugin

--- lua/cryptic/edit.lua
+++ lua/cryptic/edit.lua
@@ -11,4 +11,11 @@ return {
     'tommcdo/vim-exchange',
     commit = 'd6c1e9790bcb8df27c483a37167459bbebe0112e'
   },
+  {
+    'mbbill/undotree',
+    commit = '78b5241191852ffa9bb5da5ff2ee033160798c3b',
+    config = function ()
+      vim.keymap.set('n', '<leader>u', ':UndotreeToggle<cr>', { desc = '[U]ndo tree toggle', silent = true })
+    end
+  }
 }
