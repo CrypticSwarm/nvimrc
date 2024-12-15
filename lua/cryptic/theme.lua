388efb77d047a954dad97e5726788790c4dfcabd
CrypticSwarm <crypticswarm+git@gmail.com>
2024-12-15 16:35:17 -0600

    Change lualine file path display to relative

--- lua/cryptic/theme.lua
+++ lua/cryptic/theme.lua
@@ -11,7 +11,16 @@ return {
     'nvim-lualine/lualine.nvim',
     commit = 'b431d228b7bbcdaea818bdc3e25b8cdbe861f056',
     config = function ()
-      require('lualine').setup({})
+      require('lualine').setup({
+        sections = {
+          lualine_c = {
+            {
+              'filename',
+              path = 1,
+            }
+          }
+        }
+      })
       vim.opt.showmode = false
     end
   },
