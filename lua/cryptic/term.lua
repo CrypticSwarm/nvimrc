18be1e52a2595586cc2e5925f930e25b2d9546af
CrypticSwarm <crypticswarm+git@gmail.com>
2025-10-22 15:33:33 -0500

    Adjust Slimux commands
    Remove potential conflict on <leader>a, which will be reserved for AI
    focused commands.

--- lua/cryptic/term.lua
+++ lua/cryptic/term.lua
@@ -4,10 +4,8 @@ return {
     commit = '7056d0e12d29411dbae9591cab0c5bb798a8ff6c',
     config = function ()
       vim.keymap.set('n', '<leader>r', ':SlimuxREPLSendLine<CR>', { desc = '[R]un line', silent = true })
-      vim.keymap.set('n', '<leader>e', 'vip:SlimuxREPLSendSelection<CR>', { desc = 'Run block', silent = true })
-      vim.keymap.set('v', '<leader>r', ':SlimuxREPLSendSelection<CR>', { desc = '[R]un selection', silent = true })
-      vim.keymap.set('n', '<leader>aa', ':SlimuxShellLast<CR>', { desc = 'Select shell', silent = true })
-      vim.keymap.set('n', '<leader>ai', ':SlimuxShellPrompt<CR>', { desc = 'Shell prompt', silent = true })
+      vim.keymap.set('n', '<leader>e', 'vip:SlimuxREPLSendSelection<CR>', { desc = '[E]valuate block', silent = true })
+      vim.keymap.set('v', '<leader>e', ':SlimuxREPLSendSelection<CR>', { desc = '[E]valuate selection', silent = true })
     end
   },
 }
