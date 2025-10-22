bf42dbd5a0ab92252cafeaca0594406f35376ee3
CrypticSwarm <crypticswarm+git@gmail.com>
2025-10-22 15:34:36 -0500

    Add additional models

--- lua/cryptic/ai.lua
+++ lua/cryptic/ai.lua
@@ -5,20 +5,25 @@ return {
   config = function()
     local default_system_prompt = require("gp.defaults").chat_system_prompt
     print(default_system_prompt)
+    local ollama_model = function (display_name, model_name)
+      return {
+        name = display_name,
+        provider = "local_ollama",
+        chat = true,
+        command = false,
+        model = { model = model_name, temperature = 0.6, top_p = 1, min_p = 0.05 },
+        system_prompt = default_system_prompt
+      }
+    end
     require("gp").setup({
       openai_api_key = { 'pass', 'show', 'crypticswarm/openai-apikey' },
       providers = {
         local_ollama = { endpoint = "http://localhost:11434/v1/chat/completions", secret = ''},
       },
       agents = {
-        {
-          name = "LLaMa3.1-8B",
-          provider = "local_ollama",
-          chat = true,
-          command = false,
-          model = { model = "llama3.1:8b", temperature = 0.6, top_p = 1, min_p = 0.05 },
-          system_prompt = default_system_prompt
-        },
+        ollama_model('LLaMa3.1-8B', 'llama3.1:8b'),
+        ollama_model('GPT-OSS-20B', 'gpt-oss:20b'),
+        ollama_model('GPT-OSS-120B', 'gpt-oss:120b'),
         {
           name = "gpt-5-main",
           provider = "openai",
